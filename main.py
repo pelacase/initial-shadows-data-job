b4beeb06f630a504ebdf4b8e9573dade1b85283d
BenPela <ben@pela.earth>
2023-11-13 17:26:02 -0800

    add time only if not there

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from datetime import datetime
 
 import boto3
 import os
+import json
 
 import pytz as pytz
 
 s3 = boto3.client('s3')
 
 bucket_name = 'lomi-shadow-update-production'
-CUTOFF_DATE = datetime(2023, 10, 15, tzinfo=pytz.UTC)
+CUTOFF_DATE = datetime(2023, 11, 9, tzinfo=pytz.UTC)
+CUTOFF_DATE_LATEST = datetime(2023, 11, 11, tzinfo=pytz.UTC)
 
 s3_objects = {}
 paginator = s3.get_paginator('list_objects_v2')
@@ -17,7 +19,7 @@ for page in paginator.paginate(Bucket=bucket_name):
     files = page.get('Contents', [])
     for obj in files:
         last_modified = obj['LastModified']
-        if last_modified > CUTOFF_DATE:
+        if last_modified > CUTOFF_DATE and last_modified < CUTOFF_DATE_LATEST:
             if last_modified.year not in s3_objects:
                 s3_objects[last_modified.year] = {}
             if last_modified.month not in s3_objects[last_modified.year]:
@@ -37,9 +39,13 @@ for year in s3_objects:
                 for obj in s3_objects[year][month][day][hour]:
                     s3_object = s3.get_object(Bucket=bucket_name, Key=obj['Key'])
                     s3_object_as_string = s3_object['Body'].read().decode('utf-8')
-                    with_recieved_time = s3_object_as_string[:-1] + f', "receivedtime": "{obj["LastModified"].isoformat()}"}}\n'
+                    has_time = "receivedtime" in json.loads(s3_object_as_string)
+                    if not has_time:
+                        with_recieved_time = s3_object_as_string[:-1] + f', "receivedtime": "{obj["LastModified"].isoformat()}"}}\n'
+                    else:
+                        with_recieved_time = s3_object_as_string
                     hour_string += with_recieved_time
-                filename = f"output/year={year}/month={month}/day={day}/lomi-shadow-redshift-production-1-{year}-{month}-{day}-{hour}-00-{uuid.uuid4()}"
+                filename = f"output2/year={year}/month={month}/day={day}/lomi-shadow-redshift-production-1-{year}-{month}-{day}-{hour}-00-{uuid.uuid4()}"
                 with open(filename, "w") as f:
                     f.write(hour_string)
                     print(f"written {filename}")
